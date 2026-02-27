0a64f77c16c4124c449aea571e3e839fdf19e722
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-02-27 14:58:27 +0600

    Practice swiftfullThinking 1.swift

---
+++ Swiftfull Thinking 1.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+var greeting = "Hello, playground"
+
+print(greeting)
+print(greeting)
+print(greeting)
+
+//This is a comment
+/*
+ This is a multi line comments
+ that goes to multiple lines
+ */
+let firstGreeding = "Hello, World!"
+let thisIsMyFirstGreeding = "Hello, World!"
+
+let thisIsMySecondGreeding = "Hello, World!"
