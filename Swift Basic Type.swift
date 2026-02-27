716102662fcb17868be812e17d9d37f5fddfa150
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-02-27 15:16:07 +0600

    Practicing SwiftBasicType

---
+++ Swift Basic Type.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+let myFirstItem: String = "Hello, World!"
+
+let myTitle = myFirstItem.uppercased()
+
+let mySecondItem: Bool = true
+let myThirdItem: Bool = false
+
+// Swift is a type-safe language
+
+let myFirstDate: Date = Date()
+
+//Number can be INT, Double, CGFloat & more
+
+let num1: Int = 10
+
+// Use Double for Math
+let num2: Double = 10.5
+
+// Use CGFloat for UI
+let num3: CGFloat = 10.5
