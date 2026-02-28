ed421b8ec16c0c7f19f5e09988745c71bcd51ab3
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-03-01 00:02:45 +0600

    1003 - Simple Sum

---
+++ 1003 - Simple Sum.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+if let inputA = readLine(),
+   let A = Int(inputA),
+   let inputB = readLine(),
+   let B = Int(inputB) {
+    
+    let SOMA = A + B
+    print("SOMA = \(SOMA)")
+}
